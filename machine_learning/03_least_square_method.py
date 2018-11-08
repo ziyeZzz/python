e6c5b4a4dbeec9d9ff91acb298b6e653a9e41b23
ziyeZzz <ziye123zoe@gmail.com>
2018-11-08 09:22:20 +0800

    add plot figure

--- machine_learning/03_least_square_method.py
+++ machine_learning/03_least_square_method.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # 最小二乘法求解
-# Q: y = 5 * x + 3
+# Q: y = a * x + b
 import numpy as np
+import matplotlib.pyplot as plt
 
 def lsf(x, y):
     sum_x = sum(x)
@@ -15,7 +16,12 @@ def lsf(x, y):
 
 
 if __name__ == '__main__':
-    y = np.array(1.00, 0.90, 0.90, 0.81, 0.60, 0.35)
+    y = np.array((1.00, 0.90, 0.90, 0.81, 0.60, 0.56, 0.35))
     x = np.arange(3.6, 4.2, 0.1)
     a,b = lsf(x, y)
     print('y = {} * x + {}'.format(a, b))
+    plt.plot(x, y, 'k*')
+    x1 = np.arange(3.5, 4.3, 0.01)
+    y1 = a * x1 + b
+    plt.plot(x1, y1, 'r-')
+    plt.show()
