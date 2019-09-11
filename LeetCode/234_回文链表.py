0d5c3ba1d0f57200c921abf227629b7621e207ce
administrator <zhouzy@mc2tekiMacBook-Pro.local>
2019-09-11 16:14:31 +0800

    update

--- LeetCode/234_回文链表.py
+++ LeetCode/234_回文链表.py
@@ -12,3 +12,30 @@
 进阶：
 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
 '''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def isPalindrome(self, head):
+    # find middle node
+    left_head = None
+    slow= head
+    fast = head.next
+    while fast.next:
+        left_head = slow.next
+        tmp = slow.next.next
+        left_head.next = slow
+        slow.next = tmp
+        slow = slow.next
+        fast = fast.next.next
+    
+    while slow and left_head:
+        if slow.val != left_head.val:
+            return False
+        else:
+            slow = slow.next
+            left_head = left_head.val
+    return False
+    
